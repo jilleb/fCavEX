14e5ec3f605d5aa14e94a67a9e274f6cc0aeae83
yyy257 <170938825+yyy257@users.noreply.github.com>
2024-05-14 17:48:23 +0200

    Health test: draw only 8 hearts

--- source/game/gui/screen_ingame.c
+++ source/game/gui/screen_ingame.c
@@ -420,7 +420,7 @@ static void screen_ingame_render2D(struct screen* s, int width, int height) {
 							  gstate.windows[WINDOWC_INVENTORY])),
 				  height - 32 * 8 / 5 - 23 * 2, 208, 0, 24, 24, 24 * 2, 24 * 2);
 
-	for(int k = 0; k < 10; k++) {
+	for(int k = 0; k < 8; k++) {
 		// draw hearts
 		gutil_texquad((width - 182 * 2) / 2 + k * 8 * 2,
 					  height - 32 * 8 / 5 - (22 + 10) * 2, 16, 229, 9, 9, 9 * 2,
