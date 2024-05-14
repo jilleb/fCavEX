45d4a7bc0814eb7f3940fe706db963f1a4362548
yyy257 <170938825+yyy257@users.noreply.github.com>
2024-05-14 17:15:22 +0200

    Fix segmentation fault on breaking blocks

--- source/game/gui/screen_ingame.c
+++ source/game/gui/screen_ingame.c
@@ -373,7 +373,7 @@ static void screen_ingame_render2D(struct screen* s, int width, int height) {
 		struct block_data bd
 			= world_get_block(&gstate.world, gstate.camera_hit.x,
 							  gstate.camera_hit.y, gstate.camera_hit.z);
-		if(blocks[bd.type]->onRightClick) {
+		if(bd.type && blocks[bd.type]->onRightClick) { //check if bd.type is not 0
 			icon_offset += gutil_control_icon(icon_offset, IB_ACTION2, "Use");
 		} else if(inventory_get_hotbar_item(
 					  windowc_get_latest(gstate.windows[WINDOWC_INVENTORY]),
