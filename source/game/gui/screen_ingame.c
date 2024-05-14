fbf659a33ec88a181399799eec5205c5bb155629
yyy257 <170938825+yyy257@users.noreply.github.com>
2024-05-14 19:10:58 +0200

    Split loops for drawing black and red hearts

--- source/game/gui/screen_ingame.c
+++ source/game/gui/screen_ingame.c
@@ -421,10 +421,14 @@ static void screen_ingame_render2D(struct screen* s, int width, int height) {
 				  height - 32 * 8 / 5 - 23 * 2, 208, 0, 24, 24, 24 * 2, 24 * 2);
 
 	for(int k = 0; k < 10; k++) {
-		// draw hearts
-		//gutil_texquad((width - 182 * 2) / 2 + k * 8 * 2,
-			//	  height - 32 * 8 / 5 - (22 + 10) * 2, 16, 229, 9, 9, 9 * 2,
-			//		  9 * 2);
+		// draw black hearts
+		gutil_texquad((width - 182 * 2) / 2 + k * 8 * 2,
+				  height - 32 * 8 / 5 - (22 + 10) * 2, 16, 229, 9, 9, 9 * 2,
+					  9 * 2);
+	}
+	//TODO: use health variable instead of 10
+	for (k = 0; k < 10; k++) {
+		// draw red hearts
 		gutil_texquad((width - 182 * 2) / 2 + k * 8 * 2,
 					  height - 32 * 8 / 5 - (22 + 10) * 2, 52, 229, 9, 9, 9 * 2,
 				  9 * 2);
